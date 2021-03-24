d0234f5d252dad7f528e82c20f4b6b0d1f520ed1
Tom Haddon <tom@haddons.net>
2021-03-24 14:54:50 +0100

    Create a harness object that can be reused in other tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -10,23 +10,27 @@ from charm import CharmK8SIngressCharm
 
 
 class TestCharm(unittest.TestCase):
+    def setUp(self):
+        """Setup the harness object."""
+        self.harness = Harness(CharmK8SIngressCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
     @mock.patch('charm.CharmK8SIngressCharm._define_ingress')
     @mock.patch('charm.CharmK8SIngressCharm._define_service')
     def test_config_changed(self, _define_service, _define_ingress):
-        harness = Harness(CharmK8SIngressCharm)
-        self.addCleanup(harness.cleanup)
-        harness.begin()
+        """Test our config changed handler."""
         # Confirm our _define_ingress and _define_service methods haven't been called.
         self.assertEqual(_define_ingress.call_count, 0)
         self.assertEqual(_define_service.call_count, 0)
         # Test if config-changed is called with service-name empty, our methods still
         # aren't called.
-        harness.update_config({"service-name": ""})
+        self.harness.update_config({"service-name": ""})
         self.assertEqual(_define_ingress.call_count, 0)
         self.assertEqual(_define_service.call_count, 0)
         # And now test if we set a service-name config, our methods are called.
-        harness.update_config({"service-name": "gunicorn"})
+        self.harness.update_config({"service-name": "gunicorn"})
         self.assertEqual(_define_ingress.call_count, 1)
         self.assertEqual(_define_service.call_count, 1)
         # Confirm status is as expected.
-        self.assertEqual(harness.charm.unit.status, ActiveStatus())
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())

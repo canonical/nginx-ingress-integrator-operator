b512c41d5073b95aa41bab230c707134ae721509
Tom Wardill <tom.wardill@canonical.com>
2021-05-27 11:03:49 +0100

    Fix retrieval of default None fields

--- src/charm.py
+++ src/charm.py
@@ -70,7 +70,8 @@ class NginxIngressCharm(CharmBase):
 
     def _get_config_or_relation_data(self, field, fallback):
         """Helper method to get data from config or the ingress relation."""
-        config_data = self.config[field]
+        # Config fields with a default of None don't appear in the dict
+        config_data = self.config.get(field, None)
         # A value of False is valid in these fields, so check it's not a null-value instead
         if field in BOOLEAN_CONFIG_FIELDS and (config_data is not None and config_data != ""):
             return config_data

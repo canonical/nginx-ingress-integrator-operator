dc32291596f8f9ca74fe194ccaa71af201fe5841
jdkandersson <david.andersson@canonical.com>
2022-09-05 16:27:06 +1000

    Style: fix linting errors and update copyright notice

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
@@ -13,7 +12,7 @@ from ops.model import ActiveStatus, Application
 @pytest.mark.abort_on_fail
 async def test_active(app: Application):
     """
-    arrange: given charm has been built and connected to another unit
+    arrange: given charm has been built, deployed and related to a dependent application
     act: when the status is checked
     assert: then the workload status is active.
     """
@@ -24,14 +23,16 @@ async def test_active(app: Application):
 @pytest.mark.abort_on_fail
 async def test_reachable(app: Application):
     """
-    arrange: given charm has been built and connected to another unit
-    act: when other unit-s service is queried via the ingress
-    assert: then the reasponse is HTTP 200 OK.
+    arrange: given charm has been built, deployed and related to a dependent application
+    act: when the dependent application is queried via the ingress
+    assert: then the response is HTTP 200 OK.
     """
     # Get the IP address which is in the status message
     ip_regex = r"[0-9]+(?:\.[0-9]+){3}"
     ip_address_match = re.search(ip_regex, app.units[0].workload_status_message)
-    assert ip_address_match, "could not find IP address in status message"
+    assert (
+        ip_address_match
+    ), f"could not find IP address in status message: {app.units[0].workload_status_message}"
     ip_address = ip_address_match.group(0)
     port = "8080"
 

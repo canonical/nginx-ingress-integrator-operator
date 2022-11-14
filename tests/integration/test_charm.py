552c2e33d832d9a9ae986456b067c8a061dca0e9
Franco Forneron <franco.forneron@canonical.com>
2022-11-14 12:13:41 -0300

    adding integration tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 
 import re
+import subprocess
 import time
 
 import pytest
@@ -22,10 +23,10 @@ async def test_active(app: Application):
 
 @pytest.mark.asyncio
 @pytest.mark.abort_on_fail
-async def test_reachable(app: Application):
+async def test_service_reachable(app: Application):
     """
     arrange: given charm has been built, deployed and related to a dependent application
-    act: when the dependent application is queried via the ingress
+    act: when the dependent application is queried via the service
     assert: then the response is HTTP 200 OK.
     """
     # Get the IP address which is in the status message
@@ -39,5 +40,29 @@ async def test_reachable(app: Application):
     port = "8080"
 
     response = requests.get(f"http://{ip_address}:{port}")
+
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+@pytest.mark.abort_on_fail
+async def test_ingress_reachable_ip_status_msg(app: Application):
+    """
+    arrange: given charm has been built, deployed and related to a dependent application
+    act: when the dependent application is queried via the ingress
+    assert: then the response is HTTP 200 OK.
+    """
+    # Get the IP address which is in the status message
+    ip_regex = r"[0-9]+(?:\.[0-9]+){3}"
+    ip_address_match = re.findall(ip_regex, app.units[0].workload_status_message)
+    assert (
+        ip_address_match
+    ), f"could not find IP address in status message: {app.units[0].workload_status_message}"
+    ip_address = ip_address_match[0]
+    port = "80"
+    ps = subprocess.Popen(['echo', f'{ip_address} hello-kubecon'], stdout=subprocess.PIPE)
+    subprocess.run(['sudo', 'tee', '-a' ,'/etc/hosts'], stdin=ps.stdout)
+
+    response = requests.get(f"http://hello-kubecon:{port}")
 
     assert response.status_code == 200

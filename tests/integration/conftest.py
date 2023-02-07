624c849df60d0aa1e41416682e18c94703885836
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-02-07 15:35:12 -0300

    Fix lint

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -71,7 +71,9 @@ async def ip_address_list(ops_test: OpsTest, app: Application):
     # Reduce the update_status frequency until the cluster is deployed
     status_message = app.units[0].workload_status_message  # type: ignore[attr-defined]
     async with ops_test.fast_forward():
-        await ops_test.model.block_until(lambda: "Ingress IP(s)" in status_message, timeout=10 * 60)
+        await ops_test.model.block_until(
+            lambda: "Ingress IP(s)" in status_message, timeout=10 * 60
+        )
     ip_regex = r"[0-9]+(?:\.[0-9]+){3}"
     ip_address_list = re.findall(ip_regex, status_message)
     assert ip_address_list, f"could not find IP address in status message: {status_message}"

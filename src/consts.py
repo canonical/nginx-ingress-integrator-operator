cc1502f3dcf4177547ea87de7d2583bd1756037c
Weii Wang <weii.wang@canonical.com>
2023-10-26 23:59:35 +0800

    Introducing standard ingress relation support

---
+++ src/consts.py
@@ -0,0 +1,7 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Constants used by the nginx-ingress-integrator charm."""
+
+CREATED_BY_LABEL = "nginx-ingress-integrator.charm.juju.is/managed-by"
+BOOLEAN_CONFIG_FIELDS = ["rewrite-enabled"]
---
+++ tests/unit/constants.py
@@ -0,0 +1,6 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""nginx-ingress-integrator charm unit test constants."""
+
+TEST_NAMESPACE = "test"

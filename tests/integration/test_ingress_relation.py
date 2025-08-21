6f2508feb5999eabdfb20c9d50ff96f48462bfcc
florentianayuwono <florentiana.yuwono@canonical.com>
2025-08-21 16:01:09 +0700

    fix route path

--- tests/integration/test_ingress_relation.py
+++ tests/integration/test_ingress_relation.py
@@ -84,7 +84,9 @@ async def test_ingress_relation(
     await model.wait_for_idle(status="active")
     await run_action("any", "rpc", method="start_server")
 
-    response = requests.get("http://127.0.0.1/path/ok", headers={"Host": "any"}, timeout=5)
+    response = requests.get(
+        f"http://127.0.0.1/{model.name}-any/ok", headers={"Host": "any"}, timeout=5
+    )
     assert response.status_code == 200
     assert response.text == "http://any/"
 

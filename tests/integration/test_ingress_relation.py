308c7c0cd284218de641f231f65619ed6424eff8
florentianayuwono <florentiana.yuwono@canonical.com>
2025-08-22 15:38:42 +0700

    add relation

--- tests/integration/test_ingress_relation.py
+++ tests/integration/test_ingress_relation.py
@@ -95,6 +95,7 @@ async def test_ingress_relation(
     src_overwrite["any_charm.py"] = make_any_charm_source(strip_prefix=True)
     await charm.set_config({"src_overwrite": src_overwrite})
     await model.wait_for_idle()
+    await model.add_relation("any:ingress", "ingress:ingress")
     await model.wait_for_idle(status="active")
     await run_action("any", "rpc", method="start_server")
 

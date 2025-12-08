9b34e6405199361d922a5b8079a569aa24acf9b4
James Garner <james.garner@canonical.com>
2025-12-08 16:30:35 +1300

    fix: correct bad type annotation that wasn't caught previously

--- src/charm.py
+++ src/charm.py
@@ -399,7 +399,7 @@ class NginxIngressCharm(CharmBase):
         relation = self.model.get_relation(TLS_CERT)
         return relation
 
-    def _get_certificate_requests(self) -> List[str]:
+    def _get_certificate_requests(self) -> List[CertificateRequestAttributes]:
         """Get the certificate requests for the charm."""
         hostnames = self.get_all_hostnames()
         return [
